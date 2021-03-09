374f44a3ac5722e49a2413a99c249fe48f45b653
wass01 <maatallawassim01@gmail.com>
2021-03-09 14:50:11 +0100

    creazione prime funzioni

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// Il computer deve generare 16 numeri casuali tra 1 e 100.
+// I numeri non possono essere duplicati.
+// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// L’utente non può inserire più volte lo stesso numero.
+// Se il numero è presente nella lista dei numeri generati, la partita termina,
+// altrimenti si continua chiedendo all’utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero “vietato”
+// o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio,
+//  cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+var randArray = [];
+
+
+
+// functions
+
+function genNum(array, max){
+  while (array.lenght < 16) {
+    var randNum = randomNumber(1, max);
+    if (!array.includes(randNum)) {
+      array.push(randNum)
+    }
+  }
+}
+
+
+
+function randomNumber(min,max){
+  return parseInt(Math.random() * (max - min) + min);
+}
